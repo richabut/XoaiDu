e4d760e0c94064302f732ecf90336832426b9cbe
Richard Butlewski <richard.butlewski@gmail.com>
2026-08-03 22:07:13 +0200

    Initial Xoai Du MVP

---
+++ app/menu/page.tsx
@@ -0,0 +1,13 @@
+export default function MenuPage() {
+  return (
+    <main>
+      <h1>Xoài Đu Menu</h1>
+
+      <p>Bún Chả - 139.000 ₫</p>
+
+      <p>Bánh Xèo Tôm Bò - 169.000 ₫</p>
+
+      <p>Nem Nấm - 75.000 ₫</p>
+    </main>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>XOÀI ĐU Bistro & Cafe</h1>
+
+      <p>Authentic Vietnamese Cuisine</p>
+
+      <p>Welcome to Xoài Đu</p>
+
+      <p>Freshly prepared Vietnamese cuisine in Tây Hồ.</p>
+
+      <p>
+        19 Ng. 5 P. Từ Hoa, Tây Hồ, Hà Nội 124000
+      </p>
+
+      <p>+84 902 283 255</p>
+
+      <p>xoaidu2022@gmail.com</p>
+    </main>
+  );
+}
