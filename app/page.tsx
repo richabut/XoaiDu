28d27f3daed5e0c4bb47154b043ff7f017434e7b
Richard Butlewski <richard.butlewski@gmail.com>
2026-08-03 22:47:38 +0200

    Working Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
 export default function Home() {
   return (
     <main>
-      <h1>XOÀI ĐU Bistro & Cafe</h1>
+      <h1>XOAI DU Bistro and Cafe</h1>
 
       <p>Authentic Vietnamese Cuisine</p>
 
-      <p>Welcome to Xoài Đu</p>
-
-      <p>Freshly prepared Vietnamese cuisine in Tây Hồ.</p>
-
-      <p>
-        19 Ng. 5 P. Từ Hoa, Tây Hồ, Hà Nội 124000
-      </p>
+      <p>19 Ng. 5 P. Tu Hoa, Tay Ho, Ha Noi</p>
 
       <p>+84 902 283 255</p>
 
