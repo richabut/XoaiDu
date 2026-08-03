779f2fe0ab182e1a1dcbe8cf6f408d835b8d9f0b
Richard Butlewski <richard.butlewski@gmail.com>
2026-08-03 22:59:53 +0200

    Added menu cart and checkout pages

---
+++ app/checkout/page.tsx
@@ -0,0 +1,33 @@
+export default function CheckoutPage() {
+  return (
+    <main>
+      <h1>Checkout</h1>
+
+      <h2>Customer Information</h2>
+
+      <p>First Name</p>
+
+      <p>Last Name</p>
+
+      <p>Phone Number *</p>
+
+      <p>Email (optional)</p>
+
+      <h2>Delivery Information</h2>
+
+      <p>Address</p>
+
+      <p>Delivery Notes</p>
+
+      <p>
+        ☐ I confirm that the map location matches my delivery address
+      </p>
+
+      <h2>Payment</h2>
+
+      <p>○ Cash</p>
+
+      <p>○ QR Code</p>
+    </main>
+  );
+}
